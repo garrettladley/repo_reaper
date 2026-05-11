a7e12fc4cacdf760694d4456422185179876ad6d
Garrett Mitchell Ladley <garrett.ladley14@gmail.com>
2026-05-11 12:25:40 -0400

    fix(search): remove bm25 config file dependency

--- crates/core/src/error.rs
+++ crates/core/src/error.rs
@@ -1,12 +1,4 @@
 #[derive(Debug, thiserror::Error)]
-pub enum RankingError {
-    #[error("failed to load BM25 configuration")]
-    ConfigLoad(#[from] config::ConfigError),
-    #[error("failed to determine current directory")]
-    CurrentDir(#[source] std::io::Error),
-}
-
-#[derive(Debug, thiserror::Error)]
 pub enum EvalError {
     #[error("failed to parse evaluation data")]
     Parse(#[source] serde_json::Error),
